6f5c1056d2733e316e2b67de354aa672bdd9fdb2
huawei <809870131@qq.com>
2020-09-17 14:40:35 +0800

    优化

--- app/Models/SpellConfigModel.php
+++ app/Models/SpellConfigModel.php
@@ -4,35 +4,11 @@
 namespace App\Models;
 
 
-use Mockery\Exception;
-
 class SpellConfigModel extends BaseModel
 {
     protected $table = 'spell_config';
 
     public $timestamps = false;
-
-    protected $casts = [
-        'value' => 'json',
-    ];
-
-    public function getValueAttribute($value)
-    {
-        try{
-            return array_values(json_decode($value, true) ?: []);
-        }catch (Exception $e) {
-            return $value;
-        }
-    }
-
-    public function setValueAttribute($value)
-    {
-        if (is_array(($value))) {
-            $this->attributes['value'] = json_encode(array_values($value));
-        } else {
-            $this->attributes['value'] = $value;
-        }
-    }
 
     public function setImageAttribute($value)
     {
